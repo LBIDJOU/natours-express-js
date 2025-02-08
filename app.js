e87112357df81d949b47f5ab018191f3daf8b7ce
LBIDJOU <brijja.naime5@gmail.com>
2025-02-08 01:29:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const bookingController = require('./controllers/bookingController');
 const app = express();
 
 // 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
